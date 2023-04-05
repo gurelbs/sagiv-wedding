cf228e52aa17779ba1857d97607f4667af666672
gurel <guri240@gmail.com>
2023-04-05 14:35:52 +0300

    add icons

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import config from './main.config';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -8,6 +8,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class MainComponent {
   constructor(private sanitizer: DomSanitizer) { }
+
+  config = config;
 
   getSafeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
---
+++ src/app/main/main.config.ts
@@ -0,0 +1,6 @@
+export default {
+    location: {
+        google: 'https://maps.app.goo.gl/p2MjCnkGxfQUaDRJ6',
+        waze: 'https://waze.com/ul/hsvbb646ge'
+    }
+}
