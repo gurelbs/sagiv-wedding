c46ea7f4a7ca0544a170db0cd1fbd7ebd8976028
gurel <guri240@gmail.com>
2023-04-11 17:45:13 +0300

    remove old list

--- src/app/all-guests/all-guests.component.ts
+++ src/app/all-guests/all-guests.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { downloadExcel } from './export-excel';
+import { isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-all-guests',
   templateUrl: './all-guests.component.html',
   styleUrls: ['./all-guests.component.scss']
 })
-export class AllGuestsComponent implements OnInit {
-  constructor(private http: HttpClient){}
+export class AllGuestsComponent implements OnInit, OnDestroy {
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object){}
+  private httpSubscription!: Subscription;
   allGuests: any;
+
   ngOnInit(): void {
-    this.http.get('/all-guests').subscribe(
-      (res: any) => {
-        this.allGuests = res.allGuests;
-      },
-      (error) => {
-        console.error('Failed to save RSVP', error);
-      }
-    );  
+    if (isPlatformBrowser(this.platformId)) {
+      this.httpSubscription = this.http.get('/all-guests').subscribe({
+        next: (data: any) => {
+          this.allGuests = data.allGuests;
+        },
+        error: (error: any) => {
+          console.error(error);
+        },
+        complete: () => {
+          console.log('HTTP request completed.');
+        }
+      })
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe();
+    }
   }
 
   downloadExcel(){
